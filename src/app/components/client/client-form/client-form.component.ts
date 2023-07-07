7cd364f221521ea847ad466cafed1591e1257dfc
gezierri <gezierrismaniotto@gmail.com>
2023-07-07 13:13:15 -0300

    create validation to client form

--- src/app/components/client/client-form/client-form.component.ts
+++ src/app/components/client/client-form/client-form.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Client } from 'src/app/model/client';
+import { ClientsService } from 'src/app/services/clients.service';
+
+class ClientImpl implements Client {
+  id: string = '';
+  name: string = '';
+  email: string = '';
+  dateBirth: Date = new Date();
+  phoneNumber: string = '';
+  street: string = '';
+  number: string = '';
+  complement: string = '';
+  neighborhood: string = '';
+  city: string = '';
+  state: string = '';
+  country: string = '';
+}
 
 @Component({
   selector: 'app-client-form',
   templateUrl: './client-form.component.html',
-  styleUrls: ['./client-form.component.css']
+  styleUrls: ['./client-form.component.css'],
 })
 export class ClientFormComponent {
-  value1: string = '47991766414'
-  date: Date = new Date()
+  clientForm: FormGroup;
+  submitted = false;
+  client: ClientImpl = new ClientImpl();
+
+  value1: string = '47991766414';
+  date: Date = new Date();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private clientsService: ClientsService
+  ) {
+    this.clientForm = this.formBuilder.group({
+      client_name: ['', Validators.required]
+    });
+  }
+
+  public onSubmit(): void {
+    this.submitted = true;
+    if(this.clientForm.invalid){
+      return;
+    }
+  }
 }
--- src/app/components/client/client.module.ts
+++ src/app/components/client/client.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { ClientRoutingModule } from './client-routing.module';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { ClientFormComponent } from './client-form/client-form.component';
 import { ClientComponent } from './list/client.component';
@@ -18,7 +18,8 @@ import { CalendarModule } from 'primeng/calendar';
     NgxPaginationModule,
     FormsModule,
     InputMaskModule,
-    CalendarModule
+    CalendarModule,
+    ReactiveFormsModule
   ],
 })
 export class ClientModule {}
